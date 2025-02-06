464e4a41d4b6019277e00cab170aa34f2e846f97
LeandrojLemes <leandroolemes22@gmail.com>
2025-02-05 22:30:47 -0300

    Criação dos primeiros Components

---
+++ ProjetoTerceiraFase/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Body from './Components/Body'
+import Demo1 from './Components/Demo1'
+import Header from './Components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <Body/>
+      
+    </>
+  
+  )
+}
+
+export default App
---
+++ ProjetoTerceiraFase/src/Components/Body.jsx
@@ -0,0 +1,14 @@
+import './Body.css'
+import Demo1 from './Demo1';
+
+function Body() {
+  return (
+    <div className='container-body'>
+        <p></p>
+        <center><h1>Página Principal</h1></center>
+        <Demo1/>
+    </div>
+  )
+};
+
+export default Body;
