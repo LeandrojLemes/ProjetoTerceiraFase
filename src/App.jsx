a2849d21e1ce00499390f69f41944bf0cbc04d2e
LeandrojLemes <leandroolemes22@gmail.com>
2025-02-19 21:03:02 -0300

    Jogo de emojis

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
-import Body from './Components/Body'
-import Demo1 from './Components/Demo1'
-import Header from './Components/Header'
+// import Body from './Components/Body'
+// import Demo1 from './Components/Demo1'
+// import Header from './Components/Header'
 
 function App() {
 
+  const [emojis, setEmojis] = useState([
+    '😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊',
+    '😋', '😎', '😍', '😘', '🥰', '😗', '😙', '😚', '🙂', '🤗',
+    '🤩', '🤔', '🤨', '😐', '😑', '😶', '🙄', '😏', '😣', '😥'
+  ]);
+  
+  const [emoji, setEmoji] = useState('😥')
+
+  function trocarEmoji(){
+    if(emojis.length > 0){
+    let i = Math.floor(Math.random() * emojis.length)
+    setEmoji(emojis[i])
+    setEmojis(emojis.filter((e) => e != emojis[i] ))
+
+    useEffect(() => [])
+    }else{
+      alert ('Acabou!')
+    }
+  }
+
   return (
-    <>
-      <Header/>
-      <Body/>
-      
-    </>
+    <div className='container-app'>
+      <p className='emoji' onClick={trocarEmoji}>
+        Clique no emoji!
+          {emoji}
+      </p>
+
+    </div>
+  
   
   )
 }
 
-export default App
+export default App;
